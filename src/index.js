242397a02e00cd33f71b91935ab6cda7f6e3c87a
taukitianin <newvivan@gmail.com>
2018-09-23 18:34:48 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,31 +1,48 @@
+
 class Sorter {
   constructor() {
-    // your implementation
+    this.elems = [];
+    this.forSort = [];
+    this.compareFunction = function(l,r){
+        return l - r;
+    }
+    
   }
-
+  
   add(element) {
-    // your implementation
+    this.elems.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.elems[index];
   }
 
   get length() {
-    // your implementation
+    return this.elems.length;
   }
 
   toArray() {
-    // your implementation
+    return this.elems;
   }
 
   sort(indices) {
-    // your implementation
+    indices.sort(function(l,r){
+      return l - r;
+  });
+    for(let i=0; i<indices.length; i++){
+      this.forSort.push(this.elems[indices[i]]);
+    }
+    //console.log(this.forSort);
+    this.forSort.sort(this.compareFunction);
+    //console.log(this.forSort);
+    for(let i=0; i<indices.length; i++){
+      this.elems[indices[i]] = this.forSort[i];
+    }
+    this.forSort = [];
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compareFunction = compareFunction;
   }
 }
-
-module.exports = Sorter;
+module.exports=Sorter;
